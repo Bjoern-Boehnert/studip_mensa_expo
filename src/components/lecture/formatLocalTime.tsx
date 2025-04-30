c515f7999daf556eab329f1f25f742f2a63adced
Bjoern-Boehnert <bboehnert@web.de>
2025-04-30 23:59:27 +0200

    add time helper functions

--- src/components/lecture/formatLocalTime.tsx
+++
@@ -1,8 +0,0 @@
-import { formatInTimeZone } from "date-fns-tz";
-import { de } from "date-fns/locale";
-
-export const formatLocalTime = (unixTimestamp: number, formatStr: string): string => {
-	return formatInTimeZone(unixTimestamp * 1000, "Europe/Berlin", formatStr, {
-		locale: de,
-	});
-};
